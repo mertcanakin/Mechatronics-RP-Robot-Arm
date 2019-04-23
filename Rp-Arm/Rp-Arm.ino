a855b6b521b469858ba7326b5f8de381ad56ee13
Mertcan Akin <mertcanakin95@gmail.com>
2019-04-23 22:34:35 +0300

    Oteleme hareketi eklendi ve home pozisyonuna gelme eklendi.Duzenlemeler bitmedi.

--- Rp-Arm/Rp-Arm.ino
+++ Rp-Arm/Rp-Arm.ino
@@ -14,10 +14,13 @@ static const int STEPS_PER_REVOLUTION = 32 * 64;
 float hipo;     //öteleme için hipotenus
 float devir;    //step motorun donmesi 
 float angle;    //step motorun dönmesi
+
 int x=150;
 int y=68;
 
-
+float angle2;
+float hipo2;
+float devir2;
 tardate::X113647Stepper myStepper(
   STEPS_PER_REVOLUTION,
   in1, in2, in3, in4);
@@ -31,7 +34,7 @@ void setup(){
 
 void loop(){
 
-angle=atan2(x,y);     //Servonun döneceği açı miktarının radyan cinsinden hesaplanması.
+angle=atan2(y,x);     //Servonun döneceği açı miktarının radyan cinsinden hesaplanması.
 float angle_degree=57.29577*angle;    //radyanın dereceye çevrilmesi.
 Serial.print("Servonun aci degeri: ");
 Serial.println(angle_degree);
@@ -45,5 +48,46 @@ Serial.println(devir);
 myStepper.step(devir);      //step motorun döndürülmesi
 delay(200);
 Serial.println("Istenen konuma gelindi");
+
+
+angle=atan2(y,(x+50));     //Servonun öteleme yapacağı açı miktarı
+angle_degree=57.29577*angle;    //radyanın dereceye çevrilmesi.
+Serial.print("Servonun 2.aci degeri: ");
+Serial.println(angle_degree);
+r_servo.write(angle_degree); 
+delay(2000);
+
+
+hipo=sqrt(50*50+y*y);     //Oteleme icin hipotenusun hesaplanması
+devir=62.83*hipo;    //step motorun döndürmesi gereken açı miktarınn hesaplanması
+Serial.print("Step motorun 2. ötelemesi :");
+Serial.println(devir);
+myStepper.step(devir);      //step motorun döndürülmesi
+Serial.print(hipo);
+delay(200);
+Serial.println("Oteleme yapildi.");
+
+angle=atan2(y+50,x);     //Servonun öteleme yapacağı açı miktarı
+angle_degree=57.29577*angle;    //radyanın dereceye çevrilmesi.
+Serial.print("Servonun 3.aci degeri: ");
+Serial.println(angle_degree);
+r_servo.write(angle_degree); 
+delay(2000);
+
+
+hipo=sqrt(x*x+50*50);     //Oteleme icin hipotenusun hesaplanması
+devir=62.83*hipo;    //step motorun döndürmesi gereken açı miktarınn hesaplanması
+Serial.print("Step motorun 3. ötelemesi :");
+Serial.println(devir);
+myStepper.step(devir);      //step motorun döndürülmesi
+Serial.print(hipo);
+delay(200);
+Serial.println("Oteleme yapildi.");
+
+r_servo.write(0);
+//limit switch eklencek.
+//öteleme için 50/50 hipotenüs kullan.
+Serial.println("Home pozisyonuna gelindi.");
+
 
 }
